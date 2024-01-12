f08f36ae8bbbf2848bea2c96c19efa6fb2f32e99
qus0in <qus0in@gmail.com>
2024-01-12 12:45:29 +0900

    Update keyword list in data.py

--- src/data.py
+++ src/data.py
@@ -16,7 +16,7 @@ def get_marketcap_from_naver() -> pd.DataFrame:
     cols = ('itemcode', 'etfTabCode', 'itemname', 'amonut', 'marketSum')
     data = response.json().get('result').get('etfItemList')
     df = pd.DataFrame(data).dropna().loc[:, cols]
-    kwds = '액티브|혼합|레버리지|2X|단기|금리|배당|3년|은행|BBIG|글로벌'\
+    kwds = '액티브|혼합|레버리지|2X|단기|금리|배당|3년|은행|BBIG|글로벌|헬스케어|바이오'\
             +'|인도|베트남|니케이|콜|TR|닥100|P500|리츠|MSCI|R50|I300'\
             +'|HANARO|SOL|ARIRANG'
     df.query(f'not itemname.str.contains("{kwds}")', inplace=True)
