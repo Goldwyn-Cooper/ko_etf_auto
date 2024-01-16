73fcf046dc850852aa39800cac0c52df20e8164e
qus0in <qus0in@gmail.com>
2024-01-16 11:14:18 +0900

    Refactor get_marketcap_from_naver function to filter out unwanted ETFs

--- src/data.py
+++ src/data.py
@@ -13,27 +13,30 @@ PRICE='https://api.finance.naver.com/siseJson.naver'
 
 def get_marketcap_from_naver() -> pd.DataFrame:
     response = requests.get(MARKETCAP) 
-    cols = ('itemcode', 'etfTabCode', 'itemname', 'amonut', 'marketSum')
+    cols = ('itemcode', 'etfTabCode', 'itemname', 'amonut', 'quant', 'marketSum')
     data = response.json().get('result').get('etfItemList')
     df = pd.DataFrame(data).dropna().loc[:, cols]
+    kwds_pre = '레버리지|2X'\
+            +'|HANARO|KOSEF|SOL|ARIRANG'
+    df.query(f'not itemname.str.contains("{kwds_pre}")',
+             inplace=True)
     df['category_marketSum_median'] = df['etfTabCode'].apply(
         lambda x: df[df.etfTabCode == x].marketSum.median())
-    df['category_amonut_mean'] = df['etfTabCode'].apply(
-        lambda x: df[df.etfTabCode == x].amonut.mean())
-    kwds = '액티브|혼합|합성|TR'\
+    df['category_amonut_median'] = df['etfTabCode'].apply(
+        lambda x: df[df.etfTabCode == x].amonut.median())
+    df['category_quant_mean'] = df['etfTabCode'].apply(
+        lambda x: df[df.etfTabCode == x].quant.mean())
+    kwds = '액티브|혼합|합성|TR|콜'\
             +'|3년|10년|단기|장기|금리'\
-            +'|배당|가치|은행|증권|리츠'\
-            +'|BBIG|ESG|삼성|메타버스|소재'
+            +'|배당|은행|증권|리츠'\
+            +'|BBIG|ESG|삼성|메타버스|소재|\sTOP10$'
     df.query(f'not itemname.str.contains("{kwds}")\
-               or etfTabCode == 3\
-               or itemname.str.contains("엔선물|달러선물")',
+               or itemname.str.contains("엔선물|달러선물|TOP10인버스")',
                inplace=True)
-    kwds2 = '레버리지|2X'\
-            +'|HANARO|KOSEF|SOL|ARIRANG'
-    df.query(f'not itemname.str.contains("{kwds2}")',
-             inplace=True)
     expr = f'marketSum >= category_marketSum_median\
-            and (amonut >= category_amonut_mean or etfTabCode == 7)\
+            and ((amonut >= category_amonut_median\
+            and (quant >= category_quant_mean or etfTabCode == 3))\
+            or etfTabCode == 7)\
             and etfTabCode not in [1, 4]'
     return df.query(expr).loc[:, ['itemcode', 'itemname']].reset_index(drop=True)
 
