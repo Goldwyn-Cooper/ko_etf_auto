842d9f4825683ef5ddb2738ee1bdcb5fc5ff7a62
qus0in <qus0in@gmail.com>
2024-01-18 12:41:31 +0900

    Refactor data.py to calculate mean instead of median for category_amonut_median

--- src/data.py
+++ src/data.py
@@ -16,16 +16,17 @@ def get_marketcap_from_naver() -> pd.DataFrame:
     cols = ('itemcode', 'etfTabCode', 'itemname', 'amonut', 'quant', 'marketSum')
     data = response.json().get('result').get('etfItemList')
     df = pd.DataFrame(data).dropna().loc[:, cols]
-    # 시가총액 & 거래금액 상위 50% 이상, 거래량 평균 이상
+    # 시가총액 상위 50% 이상, 거래금액 & 거래량 평균 이상
     # 2배 레버리지 제외, 국내지수, 국내파생, 해외 제외
     # but 코스피, 코스닥, 2차전지 인버스 포함
     df['category_marketSum_median'] = df['etfTabCode'].apply(
         lambda x: df[df.etfTabCode == x].marketSum.median())
     df['category_amonut_median'] = df['etfTabCode'].apply(
-        lambda x: df[df.etfTabCode == x].amonut.median())
+        lambda x: df[df.etfTabCode == x].amonut.mean())
     df['category_quant_mean'] = df['etfTabCode'].apply(
         lambda x: df[df.etfTabCode == x].quant.mean())
     kwds = '액티브|혼합|합성|TR|콜|플러스|포커스'\
+            +'|ARIRANG|SOL|HANARO'\
             +'|3년|10년|단기|장기|금리|배당|은행|증권|리츠'\
             +'|BBIG|ESG|메타버스|삼성|소재|소부장'
     df.query(f'not itemname.str.contains("{kwds}")\
